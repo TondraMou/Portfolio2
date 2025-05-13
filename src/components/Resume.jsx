e34f449a4cfb85f5adfd73a2ad96e45550a6d3ab
TondraMou <tondramou3@gmail.com>
2025-05-13 20:30:53 +0600

    added resume

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Resume = ({center}) => {
     return (
         <>
-            <Link to="my-resume.pdf" target="_blank">
+            <Link to="Tondra-Resume.pdf" target="_blank">
             <button className={`btn btn-outline border-2 border-purple-600 flex gap-x-2 items-center text-purple-600 md:px-4 hover:bg-purple-600 hover:border-none hover:text-white font-bold ${center && "block md:mx-0 mx-auto"}`}><FaDownload className="md:block hidden text-lg" /> <span className="md:block hidden">Download</span>Resume</button>
             </Link>
         </>
